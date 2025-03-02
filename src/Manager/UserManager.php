9a663d3ad7d0fc77c65bd733eb7e7ecbd9938fb8
Kadu Fernandes <kadu.fernandes.pt@gmail.com>
2025-03-02 17:03:52 +0000

    Implement UserManager

---
+++ src/Manager/UserManager.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Manager;
+
+use App\Entity\User;
+use App\Exceptions\UserExistsException;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Validator\Exception\ValidationFailedException;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class UserManager
+{
+    public function __construct(
+        private readonly EntityManagerInterface $entityManager,
+        private readonly UserPasswordHasherInterface $passwordHasher,
+        private readonly ValidatorInterface $validator
+    ) {
+
+    }
+
+    /**
+     * @throws UserExistsException
+     * @throws ValidationFailedException
+     */
+    public function createUser(User $user): User
+    {
+        // The controller already validate this, but lets assume somebody else decided to use this manager elsewhere.
+        if (null !== $user->getId() ?? null) {
+            throw new UserExistsException();
+        }
+
+        // The controller already validate this, but lets assume somebody else decided to use this manager elsewhere.
+        $errors = $this->validator->validate($user);
+        if (count($errors) > 0) {
+            throw new ValidationFailedException($user, $errors);
+        }
+
+        $hash = $this->passwordHasher->hashPassword($user, $user->getPassword());
+        $user->setPassword($hash);
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        return $user;
+    }
+
+    public function deleteUser(User $user): void
+    {
+        $this->entityManager->remove($user);
+        $this->entityManager->flush();
+    }
+}
---
+++ tests/Manager/UserManagerTest.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Tests\Manager;
+
+use App\Entity\User;
+use App\Manager\UserManager;
+use Doctrine\ORM\EntityManagerInterface;
+use PHPUnit\Framework\TestCase;
+use ReflectionClass;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Validator\ConstraintViolation;
+use Symfony\Component\Validator\ConstraintViolationList;
+use Symfony\Component\Validator\Exception\ValidationFailedException;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class UserManagerTest extends TestCase
+{
+    private\ $userManager;
+
+    private $entityManager;
+
+    private $passwordHasher;
+
+    private $validator;
+
+    protected function setUp(): void
+    {
+        $this->entityManager = $this->createMock(EntityManagerInterface::class);
+        $this->passwordHasher = $this->createMock(UserPasswordHasherInterface::class);
+        $this->validator = $this->createMock(ValidatorInterface::class);
+
+        $this->userManager = new UserManager(
+            $this->entityManager,
+            $this->passwordHasher,
+            $this->validator
+        );
+    }
+
+    public function testCreateUserWithInvalidDataThrowsException(): void
+    {
+        $user = new User();
+        $user->setEmail('invalid-email');
+        $user->setRoles([]);
+        $user->setPassword('123');
+
+        $violations = new ConstraintViolationList([
+            new ConstraintViolation(
+                'validation.error.email_already_exists',
+                null,
+                [],
+                $user,
+                'email',
+                $user->getEmail()
+            ),
+            new ConstraintViolation(
+                'validation.error.must_have_at_least_one_role',
+                null,
+                [],
+                $user,
+                'roles',
+                $user->getRoles()
+            ),
+            new ConstraintViolation(
+                'validation.error.the_password_must_have_at_least_x_characters',
+                null,
+                [],
+                $user,
+                'password',
+                $user->getPassword()
+            ),
+        ]);
+
+        $this->validator->method('validate')->willReturn($violations);
+
+        $this->expectException(ValidationFailedException::class);
+
+        $this->userManager->createUser($user);
+    }
+
+    public function testCreateUserHashesPasswordAndPersists(): void
+    {
+        $user = new User();
+        $user->setEmail('test@example.com');
+        $user->setRoles(['ROLE_USER']);
+        $user->setPassword('PlainTextPassword');
+
+        $this->validator->method('validate')->willReturn(new ConstraintViolationList());
+
+        $this->passwordHasher->expects($this->once())
+                ->method('hashPassword')
+                ->with($user, 'PlainTextPassword')
+                ->willReturn('hashed-password');
+
+        $this->passwordHasher->expects($this->once())
+                ->method('hashPassword')
+                ->willReturnCallback(function ($user, $password) {
+                    $user->setPassword('hashed-password');
+
+                    return 'hashed-password';
+                });
+
+        $this->entityManager->expects($this->once())->method('persist')->with($user);
+        $this->entityManager->expects($this->once())->method('flush')
+                ->willReturnCallback(function () use ($user) {
+                    $reflection = new ReflectionClass($user);
+                    $property = $reflection->getProperty('id');
+                    $property->setAccessible(true);
+                    $property->setValue($user, 1);
+                });
+
+        $createdUser = $this->userManager->createUser($user);
+
+        $this->assertEquals('hashed-password', $createdUser->getPassword(), 'Password should be hashed.');
+        $this->assertNotNull($createdUser->getId(), 'User should have an ID after creation.');
+        $this->assertEquals(1, $createdUser->getId(), 'User ID should be 1 after flush().');
+    }
+
+    public function testDeleteUserRemovesAndFlushes(): void
+    {
+        $user = new User();
+        $user->setEmail('delete@example.com');
+        $user->setRoles(['ROLE_USER']);
+        $user->setPassword('SecurePass123!');
+
+        $this->entityManager->expects($this->once())->method('remove')->with($user);
+        $this->entityManager->expects($this->once())->method('flush');
+
+        $this->userManager->deleteUser($user);
+
+        $this->assertTrue(true);
+    }
+}
